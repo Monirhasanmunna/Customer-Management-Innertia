1ce0d23b8053cce5d485f73c3b19efcb2abfacca
MH Munna <71868847+Monirhasanmunna@users.noreply.github.com>
2023-11-02 15:19:07 +0600

    add layout of admin

---
+++ resources/js/Layouts/AdminPanel.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import Navbar from './Navbar.vue';
+    import Sidebar from './Sidebar.vue'
+</script>
+
+<template>
+
+<Navbar/>
+
+<Sidebar/>
+
+
+<div class="p-4 sm:ml-64">
+   <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+      <slot></slot>
+      
+   </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
