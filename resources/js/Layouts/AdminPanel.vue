401c9180a7959e0a8b60f7e0c4e4cee753370075
MH Munna <71868847+Monirhasanmunna@users.noreply.github.com>
2023-11-02 16:19:03 +0600

    add breadcrumb

--- resources/js/Layouts/AdminPanel.vue
+++ resources/js/Layouts/AdminPanel.vue
@@ -9,12 +9,12 @@
 
 <Sidebar/>
 
-
 <div class="p-4 sm:ml-64">
-   <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-      <slot></slot>
-      
+
+   <div class="mt-16 space-y-5">
+     <slot></slot> 
    </div>
+   
 </div>
 </template>
 
---
+++ resources/js/Pages/Customer/List.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import AdminPanel from '@/Layouts/AdminPanel.vue'
+    import {Link} from '@inertiajs/vue3'
+</script>
+
+<template>
+    <AdminPanel>
+        <!-- Breadcrumb -->
+        <nav class="flex px-5 py-3 justify-end text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+            <li class="inline-flex items-center">
+            <Link :href="route('dashboard')" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                <svg class="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                </svg>
+                Dashboard
+            </Link>
+            </li>
+            <li>
+            <div class="flex items-center">
+                <svg class="w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                </svg>
+                <Link :href="route('customers.index')" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Customers</Link>
+            </div>
+            </li>
+        </ol>
+        </nav>
+        <!-- Breadcrumb -->
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+            <h3>Customers</h3>
+        </div>
+        
+    </AdminPanel>
+</template>
+
+<style scoped>
+
+</style>
