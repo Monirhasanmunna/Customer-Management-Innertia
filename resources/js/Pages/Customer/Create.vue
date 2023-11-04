1256131cc9f1d8e1c11584b4b48df5c18beafc68
MH Munna <71868847+Monirhasanmunna@users.noreply.github.com>
2023-11-04 21:32:24 +0600

    some changes

--- resources/js/Pages/Customer/Create.vue
+++ resources/js/Pages/Customer/Create.vue
@@ -20,15 +20,13 @@
         first_name : yup.string().required(),
         last_name : yup.string().required(),
         email : yup.string().email().required(),
-        phone_number : yup.string().required(),
-        // gender : yup.string().required(),
+        phone_number : yup.string().required().min(11),
         age : yup.string().required(),
     });
 
     const submit = () =>{
         form.post(route('customers.store'),{
             OnSuccess : ()=> from.reset(),
-            onError : () => console.log('error')
         });
     };
 </script>
@@ -95,10 +93,6 @@
                     <span class="text-red-500 font-semibold text-sm " v-if="form.errors.email">{{ form.errors.email }}</span>
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
-                    <Field type="text" name="address" v-model="form.address" id="floating_address" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label for="floating_address" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
-                </div>
-                <div class="relative z-0 w-full mb-6 group">
                     <Field type="number" name="age" v-model="form.age" id="floating_age" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label for="floating_age" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Age</label>
                     <span class="text-red-500 font-semibold text-sm ">{{ errors.age }}</span>
@@ -117,6 +111,10 @@
                             <option value="female">Female</option>
                         </select>
                     </div>
+                </div>
+                <div class="relative z-0 w-full mb-6 group">
+                    <Field type="text" name="address" v-model="form.address" id="floating_address" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_address" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
                 </div>
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </Form>
